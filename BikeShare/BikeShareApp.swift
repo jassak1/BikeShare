bf6a5e61b04ddb32b749f57499f0c3d1303349c6
Jassak <jassak@me.com>
2020-10-06 19:37:19 +0200

    Initial Commit

---
+++ BikeShare/BikeShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeShareApp.swift
+//  BikeShare
+//
+//  Created by Kardan on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct BikeShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
